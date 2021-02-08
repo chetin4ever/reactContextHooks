a9101ba44c1e9fb7861ffb0d42ee5b617f0b95db
chetin4ever <hetan009@gmail.com>
2021-02-08 19:38:52 +0530

    init

---
+++ src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+export class BookList extends Component {
+  render() {
+    return (
+      <div className='book-list'>
+        <ul>
+          <li>Ek hota Carver</li>
+          <li>Me Albert Aalis</li>
+          <li>Amcha Baap Ani Aamhi</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default BookList
